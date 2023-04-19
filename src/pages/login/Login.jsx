1fc61933b5bb4c797c343af3d28f7020327b0401
rgrakshit28 <rgrakshit28.professional@gmail.com>
2023-04-19 22:28:15 +0530

    Added Authentication

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,43 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [error, setError] = useState(null);
   const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    login();
+  const handleChange = (event) => {
+    setInputs(prev => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await login(inputs);
+      navigate('/');
+    } catch (error) {
+      setError(error.response.data);
+    }
   };
 
   return (
     <div className="login">
       <div className="card">
         <div className="left">
-          <h1>Hello World.</h1>
+          <h1>Social.</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
+            A platform that connects you to close ones.
+            This is the project mainly developed for showcase my skills.
+            The Project is developed with technologies like React, Node, MySql, etc.
           </p>
           <span>Don't you have an account?</span>
           <Link to="/register">
@@ -28,9 +47,11 @@ const Login = () => {
         <div className="right">
           <h1>Login</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button onClick={handleLogin}>Login</button>
+            <input required type="text" name="username" placeholder="Username" onChange={handleChange} />
+            <input required type="password" name="password" placeholder="Password" onChange={handleChange} />
+            {error &&
+              <p>{error}</p>}
+            <button type="submit" onClick={handleSubmit}>Login</button>
           </form>
         </div>
       </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,30 +1,59 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios';
 import "./register.scss";
 
 const Register = () => {
+
+  const [inputs, setInputs] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: ""
+  });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setInputs(prev => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post('/auth/register', inputs);
+      navigate("/login");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
   return (
     <div className="register">
       <div className="card">
         <div className="left">
-          <h1>Lama Social.</h1>
+          <h1>Social.</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
-            alias totam numquam ipsa exercitationem dignissimos, error nam,
-            consequatur.
+            A platform that connects you to close ones.
+            This is the project mainly developed for showcase my skills.
+            The Project is developed with technologies like React, Node, MySql, etc.
           </p>
           <span>Do you have an account?</span>
           <Link to="/login">
-          <button>Login</button>
+            <button>Login</button>
           </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+            <input required type="text" name="name" placeholder="Name" onChange={handleChange} />
+            <input required type="text" name="username" placeholder="Username" onChange={handleChange} />
+            <input required type="email" name="email" placeholder="Email" onChange={handleChange} />
+            <input required type="password" name="password" placeholder="Password" onChange={handleChange} />
+            {error &&
+              <p>{error}</p>}
+            <button type="submit" onClick={handleSubmit}>Register</button>
           </form>
         </div>
       </div>
